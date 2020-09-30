4841d07ec1931fc7925727ea4efceca2eb403fb8
ilozano <ilozano@sigma-alimentos.com>
2020-09-30 13:01:19 -0500

    added folder docs git pages

---
+++ docs/precache-manifest.e8efb6ec3cf408ae11d4853a34935eeb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "202a186bc22427f6c2479d8deae345e3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f142c06c2d4f185f4648",
+    "url": "/static/css/main.4db2cfbc.chunk.css"
+  },
+  {
+    "revision": "9b9e2434b71b12dd5f4e",
+    "url": "/static/js/2.c1682df7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.c1682df7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f142c06c2d4f185f4648",
+    "url": "/static/js/main.f973ac11.chunk.js"
+  },
+  {
+    "revision": "722a304d2cb62627ceb8",
+    "url": "/static/js/runtime-main.3cc8bee4.js"
+  }
+]);
